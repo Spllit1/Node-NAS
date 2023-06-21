3a4caafd1a596308de0bb64eb06dca630287e458
Spllit1 <106594185+Spllit1@users.noreply.github.com>
2023-06-21 13:29:16 +0200

    hey :)

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+
+app.use(express.static('public'));
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.redirect('/');
+});
+
+
+app.get('/files', (req, res) => {
+  fs.readdir('uploads', (err, files) => {
+    if (err) {
+      res.status(500).send('Error reading files');
+    } else {
+      res.json(files);
+    }
+  });
+});
+
+// Route zum Herunterladen von Dateien
+app.get('/download/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const file = path.join(__dirname, 'uploads', filename);
+  res.download(file);
+});
+
+// Route zum Umbenennen von Dateien
+app.post('/rename', express.json(), (req, res) => {
+  const oldName = path.join(__dirname, 'uploads', req.body.oldName);
+  const newName = path.join(__dirname, 'uploads', req.body.newName);
+  fs.rename(oldName, newName, (err) => {
+    if (err) {
+      res.status(500).send("Fehler beim Umbenennen der Datei");
+    } else {
+      res.send("Datei erfolgreich umbenannt");
+    }
+  });
+});
+
+// Route zum Erstellen von Ordnern
+app.post('/create-folder', express.json(), (req, res) => {
+  const folderName = path.join(__dirname, 'uploads', req.body.folderName);
+  fs.mkdir(folderName, (err) => {
+    if (err) {
+      res.status(500).send("Fehler beim Erstellen des Ordners");
+    } else {
+      res.send("Ordner erfolgreich erstellt");
+    }
+  });
+});
+
+app.get('/list', (req, res) => {
+  fs.readdir('./uploads', { withFileTypes: true }, (err, items) => {
+    if (err) {
+      res.status(500).send("Fehler beim Abrufen der Dateien");
+    } else {
+      res.json(items.map(item => ({ name: item.name, isDirectory: item.isDirectory() })));
+    }
+  });
+});
+
+
+
+
+app.delete('/delete/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const file = path.join(__dirname, 'uploads', filename);
+  fs.unlink(file, (err) => {
+    if (err) {
+      res.status(500).send("Fehler beim Löschen der Datei");
+    } else {
+      res.send("Datei erfolgreich gelöscht");
+    }
+  });
+});
+
+
+app.get('/download/:filename', (req, res) => {
+  res.download(path.join(__dirname, 'uploads', req.params.filename));
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
